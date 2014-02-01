7d30e4a97a2613f9b4a4baa1a6828a3a498b0804
David Casas Molina <david.casasmolina@gmail.com>
2014-02-01 20:47:56 +0100

    #5 #6 added models to admin dashboard

--- goz/dbapi/admin.py
+++ goz/dbapi/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from dbapi.models import Badge, Categorie, Item, Score, \
+  User, Checkin, Event, Purchase, Unlocking, Venue, Zone
 
 # Register your models here.
+admin.site.register(User)
+admin.site.register(Categorie)
+admin.site.register(Zone)
+admin.site.register(Venue)
+admin.site.register(Item)
+admin.site.register(Badge)
+admin.site.register(Score)
+admin.site.register(Checkin)
+admin.site.register(Purchase)
+admin.site.register(Unlocking)
+admin.site.register(Event)
