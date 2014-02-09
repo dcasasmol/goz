67ef9163b9384e39eff27711847bd1b155f861f1
David Casas Molina <david.casasmolina@gmail.com>
2014-02-09 01:45:15 +0100

    #1 added login view

--- goz/goz/urls.py
+++ goz/goz/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from .views import login
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'goz.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', login, name='login'),
     url(r'^admin/', include(admin.site.urls)),
+#    url(r'^fsapi/', include(foursquareapi.urls)),
 )
