a9e48d62adcc4afc3bd12390fe7d3e932d012b39
David Casas Molina <david.casasmolina@gmail.com>
2014-06-08 20:49:43 +0200

    Adds LoginRequiredMixin class

---
+++ goz/utils/mixins.py
@@ -0,0 +1,21 @@
+# foursquareapi/mixins.py
+
+from django.shortcuts import redirect
+
+class LoginRequiredMixin(object):
+  """
+    View mixin which requires that the user is authenticated.
+  """
+  not_logged_url = 'index'
+
+  def is_logged(self):
+    return self.request.user.is_authenticated()
+
+  def redirect_to_login(self, request, *args, **kwargs):
+    return redirect(self.not_logged_url)
+
+  def dispatch(self, request, *args, **kwargs):
+    if not self.is_logged():
+      return self.redirect_to_login(request, *args, **kwargs)
+
+    return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
