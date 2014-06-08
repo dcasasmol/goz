e4b3983435f9a3c8d3d1ceb27118bba3c493a372
David Casas Molina <david.casasmolina@gmail.com>
2014-06-08 20:54:57 +0200

    Add Index and Home views

--- goz/goz/urls.py
+++ goz/goz/urls.py
@@ -1,16 +1,21 @@
-from django.conf.urls import patterns, include, url
-from .views import login
+# goz/urls.py
 
 from django.contrib import admin
+from django.conf.urls import patterns, include, url
+
+from .views import Index, Home
+
+
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'goz.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+   # Examples:
+  # url(r'^$', 'goz.views.home', name='home'),
+  # url(r'^blog/', include('blog.urls')),
 
-    url(r'^$', login.as_view(), name='login'),
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^i18n/', include('django.conf.urls.i18n')),
-#    url(r'^fsapi/', include(foursquareapi.urls)),
+  url(r'^$', Index.as_view(), name='index'),
+  url(r'^i18n/', include('django.conf.urls.i18n')),
+  url(r'^admin/', include(admin.site.urls)),
+  url(r'^fsapi/', include('foursquareapi.urls')),
+  url(r'^home/', Home.as_view(), name='home'),
 )
--- goz/goz/views.py
+++ goz/goz/views.py
@@ -2,5 +2,29 @@
 
 from django.views.generic import TemplateView
 
-class login(TemplateView):
+from foursquareapi.models import FsApi
+from utils.mixins import LoginRequiredMixin
+
+class Index(TemplateView):
   template_name = 'login.html'
+
+
+class Home(LoginRequiredMixin, TemplateView):
+  template_name = 'home.html'
+
+  def get(self, request, *args, **kwargs):
+    context = self.get_context_data(**kwargs)
+
+    access_token = self.request.session.get('access_token', '')
+    if access_token:
+      api = FsApi(access_token)
+
+      user_info = api.get_user_info()
+      username = user_info['response']['user']['firstName']
+
+      context['username'] = username
+
+    else:
+      print('[ERROR] Access token not found in session')
+
+    return self.render_to_response(context)
