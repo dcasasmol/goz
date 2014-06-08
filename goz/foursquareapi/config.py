24a6f37c9e2b7cec6c5d0ef94158a070e2297601
David Casas Molina <david.casasmolina@gmail.com>
2014-06-08 20:47:55 +0200

    Adds foursquare auth and unauth config variables

---
+++ goz/foursquareapi/config.py
@@ -0,0 +1,9 @@
+# foursquareapi/config.py
+
+# Settings for foursquare login.
+CLIENT_ID = 'ZGS2B13AJH3RN03KB1O4YWEGRXAXUUZK2WVPYTA0WBZA1R22'
+CLIENT_SECRET = 'ISCNHPBWZWMOHXFN22Y1GVA3DX31QYUIFSWPHKQEV5I3ASHK'
+
+request_token_url = 'https://es.foursquare.com/oauth2/authenticate'
+access_token_url = 'https://es.foursquare.com/oauth2/access_token'
+redirect_url = 'http://localhost:8000/fsapi/callback'
